c7bb78d12c79aceea66630afff7cda3c099fa6a6
oBOLDOR <oboldorvitry@gmail.com>
2026-06-11 22:24:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,27 @@
-const CACHE = 'sakafo-v1';
-const FILES = [
-  './sakafo_app.html',
-  './manifest.json'
-];
 
+const CACHE = 'sakafo-v2';
+const FILES = [
+  '/SAKAFO-APP/index.html',
+  '/SAKAFO-APP/manifest.json'
+];
+ 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
+    caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {}))
   );
   self.skipWaiting();
 });
-
+ 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
+ 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./sakafo_app.html')))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/SAKAFO-APP/index.html')))
   );
 });
+ 
